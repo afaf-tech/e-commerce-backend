4e91df28eaa3eb152ecf4b82b1aa4120e7d69cf6
fatihalfikri5@gmail.com <fatihalfikri5@gmail.com>
2021-11-18 09:52:34 +0700

    product api

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\User;
+use App\Models\TransactionItem;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
