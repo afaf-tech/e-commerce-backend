5913ab7cc94d354ee498685193c5cfcaede179f3
fatihalfikri5@gmail.com <fatihalfikri5@gmail.com>
2021-11-18 21:24:56 +0700

    api users

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,15 @@ use App\Http\Controllers\API\ProductCategoryController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::get('products', [ProductController::class, 'all']);
 Route::get('categories', [ProductCategoryController::class, 'all']);
 
 Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('user', [UserController::class, 'fetch']);
+    Route::post('user', [UserController::class, 'updateProfile']);
+});
